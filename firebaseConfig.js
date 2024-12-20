2ca736209ec7d1de6bde2c23b07601762e2b340c
Anirudh C P <cpanirudh0@gmail.com>
2024-12-20 19:11:10 +0530

    Changed Firebase config to .env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCACdpg1DMLAEN9-FaKPw1oXptiKEw3kmc",
-  authDomain: "rotary-durbar.firebaseapp.com",
-  projectId: "rotary-durbar",
-  storageBucket: "rotary-durbar.firebasestorage.app",
-  messagingSenderId: "833557278111",
-  appId: "1:833557278111:web:e38c9bba056b9a927261aa",
-  measurementId: "G-7S5CN0PF7K",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
