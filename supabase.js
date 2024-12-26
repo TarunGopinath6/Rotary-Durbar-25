39ef68adf22456994041ad592adef0725b53ecfb
Anirudh C P <cpanirudh0@gmail.com>
2024-12-26 17:14:40 +0530

    Added Supabase Params to Config

--- supabase.js
+++ supabase.js
@@ -1,5 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(process.env.EXPO_PUBLIC_SUPABASE_URL, process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
+
+export const supabase = createClient(
+    process.env.EXPO_PUBLIC_SUPABASE_URL || "",
+    process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
+    {
+        auth: {
+            autoRefreshToken: true,
+            persistSession: true,
+        },
+    })
 
 export default supabase;
