afe83b549807ddb2f3f6bcc7eb7590416691a067
Anirudh C P <cpanirudh0@gmail.com>
2025-01-08 16:32:56 +0530

    Routing and Auth

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,18 +5,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
+import RequireAuth from "./components/RequireAuth"
+
 import Login from './pages/Login';
 import NotFoundPage from './pages/NotFoundPage';
+import Downloads from './pages/Downloads';
+import Home from './pages/Home';
+
 
 
 function App() {
   return (
     <Router>
-       <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route exact path="*" element={<NotFoundPage />} />
-      </Routes>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/home" element={<RequireAuth> <Home /> </RequireAuth>} />
+          <Route exact path="/downloads" element={<Downloads />} />
+          <Route exact path="*" element={<NotFoundPage />} />
+        </Routes>
       </LocalizationProvider>
     </Router>
   );
---
+++ admin/src/components/RequireAuth.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import supabase from "../API/supabase";
+import { auth } from "../API/firebaseConfig"
+import { useNavigate } from "react-router-dom";
+
+
+const RequireAuth = ({ children }) => {
+
+  const [authenticated, setAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkUserStatus = async (user) => {
+      if (user) {
+        try {
+          // Fetch user data from the 'members' table
+          const { data: userDoc, error: errorUserDoc } = await supabase
+            .from("members")
+            .select("*")
+            .eq("id", user.uid) // Replace 'id' with your primary key
+            .single();
+
+          if (errorUserDoc || !userDoc) {
+            console.error(
+              "Error fetching user data or user does not exist:",
+              errorUserDoc
+            );
+            navigate("/login"); // Redirect to login page
+          }
+
+          setAuthenticated(true);
+        } catch (error) {
+          console.error("Unexpected error:", error);
+          navigate("/login"); // Redirect to login page on unexpected error
+        }
+      } else {
+        navigate("/login"); // Redirect to login page if no user is logged in
+      }
+    };
+
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      checkUserStatus(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+
+  if (authenticated) {
+    return children;
+  }
+
+  // Otherwise, return null or a loading indicator while checking authentication
+  return <div>Loading...</div>;
+};
+
+export default RequireAuth;
